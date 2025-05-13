8e6123f4f82ebf6ee44951b8fac5389b60a41f86
Enzo Lopez <enzoagustinlopez2003@gmail.com>
2025-05-13 13:01:37 -0300

    primer commit

---
+++ src/test/java/com/group/mis_servicios/MisServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.mis_servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisServiciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
